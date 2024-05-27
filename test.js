2988eff08bcc7f3b715c7ed6d711f2116e427a13
Sayanjit Mukherjee <sayanjitmukherjee123@gmail.com>
2024-05-27 13:14:43 +0530

    password generator

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
-console.log('test.js loaded')
+"use strict";
+
+const passwordbox = document.getElementById("password");
+const length=12;
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+
+
+function generate() {
+    let password = "";
+    let characters = upperCase + lowerCase + numbers ;
+    for (let i = 0; i < length; i++) {
+        password += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    passwordbox.value = password;
+}
+function copy() {
+    passwordbox.select();
+    document.execCommand("copy");
+    alert("Password copied to clipboard");
+}
